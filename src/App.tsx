5c12b4b116f8b0a2bcdb405774811ba9b8bca0b4
DevCharles <charlesipn@gmail.com>
2023-07-04 18:58:34 -0600

    lesson finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Koder } from "./types/common.types";
 import KoderCard from "./components/KoderCard";
+import clsx from "clsx";
 
 export default function App() {
   const [koders, setKoders] = useState<Koder[]>([]);
-  const [koderFirstName, setKoderFirstName] = useState("");
-  const [koderLastName, setKoderLastName] = useState("");
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Koder>();
 
-  function onAddItem() {
-    if (koderFirstName && koderLastName) {
-      const koder: Koder = {
-        firstName: koderFirstName,
-        lastName: koderLastName,
-      };
-      setKoders([koder, ...koders]);
-      setKoderFirstName("");
-      setKoderLastName("");
-    } else {
-      alert("Falta llenar un campo");
-    }
-  }
-
-  function onEnter(event: React.KeyboardEvent<HTMLInputElement>) {
-    if (event.key === "Enter") {
-      onAddItem();
-    }
+  function onSubmit(data: Koder) {
+    setKoders([
+      { firstName: data.firstName, lastName: data.lastName },
+      ...koders,
+    ]);
+    reset();
   }
 
   function onDelete(indexToDelete: number) {
@@ -40,33 +34,43 @@ export default function App() {
     <main className="bg-black min-h-screen flex items-center text-white p-10 flex-col gap-10">
       <h1 className="text-3xl">Koders</h1>
 
-      <section className="flex flex-row gap-2 w-full justify-center max-w-md">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-row gap-2 w-full justify-center max-w-md"
+      >
         <input
           id="text"
           type="text"
           placeholder="Nombre"
-          className="bg-black border border-white/10 rounded w-full p-3"
-          onChange={(event) => setKoderFirstName(event.target.value)}
-          onKeyUp={onEnter}
-          value={koderFirstName}
+          className={clsx(
+            "bg-black border border-white/10",
+            "rounded w-full p-3",
+            { "border-red-500": errors.firstName }
+          )}
+          {...register("firstName", {
+            required: { value: true, message: "El nombre es requerido" },
+            minLength: {
+              value: 3,
+              message: "Minimo 3 caracteres para el nombre (tu no Xi)",
+            },
+          })}
         />
         <input
           id="text"
           type="text"
           placeholder="Apellido"
           className="bg-black border border-white/10 rounded w-full p-3"
-          onChange={(event) => setKoderLastName(event.target.value)}
-          onKeyUp={onEnter}
-          value={koderLastName}
+          {...register("lastName")}
         />
         <button
           type="submit"
           className="bg-white text-black flex items-center justify-center p-2 rounded w-1/5"
-          onClick={onAddItem}
         >
           +
         </button>
-      </section>
+      </form>
+
+      {errors.firstName && <p>{errors.firstName.message}</p>}
 
       <section className="w-full flex flex-col gap-3">
         {koders.map((koder, index) => {
